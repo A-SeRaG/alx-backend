a37cc46268f226f9ff687a9e80947c3738d59fc6
SeRaG <aserag0101@gmail.com>
2024-10-30 11:23:39 +0300

    finish

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -1,7 +1,55 @@
 #!/usr/bin/env python3
 """
-BasicCashing module
+LFUCache module
 """
 
 from base_caching import BaseCaching
 
+
+class LFUCache(BaseCaching):
+    def __init__(self):
+        """Initialize the LFU cache"""
+        super().__init__()
+        self.usage_count = {}
+        self.usage_order = []
+
+    def put(self, key, item):
+        """Add an item to the cache using LFU policy"""
+        if key is None or item is None:
+            return
+
+        if key in self.cache_data:
+            self.cache_data[key] = item
+            self.usage_count[key] += 1
+            self.usage_order.remove(key)
+            self.usage_order.append(key)
+        else:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                min_frequency = min(self.usage_count.values())
+                lfu_keys = [k for k, v in self.usage_count.items()
+                            if v == min_frequency]
+
+                if len(lfu_keys) > 1:
+                    lfu_key = next(k for k in self.usage_order
+                                   if k in lfu_keys)
+                else:
+                    lfu_key = lfu_keys[0]
+
+                del self.cache_data[lfu_key]
+                del self.usage_count[lfu_key]
+                self.usage_order.remove(lfu_key)
+                print(f"DISCARD: {lfu_key}")
+
+            self.cache_data[key] = item
+            self.usage_count[key] = 1
+            self.usage_order.append(key)
+
+    def get(self, key):
+        """Get an item from the cache and update its usage frequency"""
+        if key is None or key not in self.cache_data:
+            return None
+
+        self.usage_count[key] += 1
+        self.usage_order.remove(key)
+        self.usage_order.append(key)
+        return self.cache_data[key]
--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,7 +1,39 @@
 #!/usr/bin/env python3
 """
-BasicCashing module
+MRUCache module
 """
 
 from base_caching import BaseCaching
 
+
+class MRUCache(BaseCaching):
+    def __init__(self):
+        """Initialize the class"""
+        super().__init__()
+        self.usage_order = []
+
+    def put(self, key, item):
+        """Add an item to the cache using MRU policy"""
+        if key is None or item is None:
+            return
+
+        if key in self.cache_data:
+            self.cache_data[key] = item
+            self.usage_order.remove(key)
+            self.usage_order.append(key)
+        else:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                mru_key = self.usage_order.pop()
+                del self.cache_data[mru_key]
+                print(f"DISCARD: {mru_key}")
+
+            self.cache_data[key] = item
+            self.usage_order.append(key)
+
+    def get(self, key):
+        """Get an item from the cache, updating its usage order"""
+        if key is None or key not in self.cache_data:
+            return None
+        self.usage_order.remove(key)
+        self.usage_order.append(key)
+        return self.cache_data[key]
