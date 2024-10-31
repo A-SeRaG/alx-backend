ad3ec4a7052ebe75702fd477f9a35791b5e3e028
SeRaG <aserag0101@gmail.com>
2024-10-31 17:39:46 +0300

    last

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -17,21 +17,14 @@ class LIFOCache(BaseCaching):
 
     def put(self, key, item):
         """Add item to the cashe using LIFO"""
-        if key is None or item is None:
-            return
-
-        if key in self.cache_data:
+        if key is not None and item is not None:
             self.cache_data[key] = item
-        else:
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                oldkey = self.order.pop(3)
-                del self.cache_data[oldkey]
-                print(f"DISCARD: {oldkey}")
-            self.cache_data[key] = item
-            self.order.append(key)
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                if self.last_key:
+                    del self.cache_data[self.last_key]
+                    print(f"DISCARD: {self.last_key}")
+        self.last_key = key
 
     def get(self, key):
         """Retrive value of the key"""
-        if key is None or key not in self.cache_data:
-            return None
-        return self.cache_data[key]
+        return self.cache_data.get(key, None)
